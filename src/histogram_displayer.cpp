e92f5b0d5537772b174a7020328ebeabf9ff1067
Tomas Baca <klaxalk@gmail.com>
2019-03-26 10:12:02 +0100

    moved msgs to mrs_msgsd

--- src/histogram_displayer.cpp
+++ src/histogram_displayer.cpp
@@ -1,3 +1,4 @@
+// clang: MatousFormat
 #include <ros/ros.h>
 #include <mrs_lib/ParamLoader.h>
 #include <mrs_lib/SubscribeHandler.h>
@@ -5,10 +6,10 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-#include <mrs_bumper/Histogram.h>
+#include <mrs_msgs/Histogram.h>
 
 cv::Point cursor_pos;
-void mouse_callback([[maybe_unused]]int event, int x, int y, [[maybe_unused]]int flags, [[maybe_unused]]void* userdata)
+void mouse_callback([[maybe_unused]] int event, int x, int y, [[maybe_unused]] int flags, [[maybe_unused]] void* userdata)
 {
   cursor_pos = cv::Point(x, y);
 }
@@ -20,18 +21,15 @@ void draw_hist(const std::vector<float>& hist, cv::Mat& hist_img, unsigned highl
   const auto bins = hist.size();
   cv::minMaxLoc(hist, 0, &max_val, 0, 0);
   const unsigned height = hist_img.rows;
-  const float hscale = height/max_val;
-  const float vscale = hist_img.cols/bins;
+  const float hscale = height / max_val;
+  const float vscale = hist_img.cols / bins;
   for (unsigned b = 0; b < bins; b++)
   {
     const auto bin_val = hist.at(b);
     cv::Scalar color = cv::Scalar::all(255);
     if (b < highlight_first)
       color = cv::Scalar(255, 0, 0);
-    cv::rectangle(hist_img, cv::Point(b*vscale, height),
-                cv::Point(b*vscale+vscale, height-bin_val*hscale),
-                color,
-                -1 );
+    cv::rectangle(hist_img, cv::Point(b * vscale, height), cv::Point(b * vscale + vscale, height - bin_val * hscale), color, -1);
   }
 }
 //}
@@ -45,11 +43,11 @@ int main(int argc, char** argv)
 
   /** Create publishers and subscribers //{**/
   // Initialize other subs and pubs
-  mrs_lib::SubscribeHandlerPtr<mrs_bumper::Histogram> sh_hist;
+  mrs_lib::SubscribeHandlerPtr<mrs_msgs::Histogram> sh_hist;
 
   mrs_lib::SubscribeMgr smgr(nh);
   const bool subs_time_consistent = false;
-  sh_hist = smgr.create_handler<mrs_bumper::Histogram, subs_time_consistent>("histogram", 1, ros::TransportHints().tcpNoDelay(), ros::Duration(5.0));
+  sh_hist = smgr.create_handler<mrs_msgs::Histogram, subs_time_consistent>("histogram", 1, ros::TransportHints().tcpNoDelay(), ros::Duration(5.0));
 
   if (!smgr.loaded_successfully())
   {
@@ -65,7 +63,7 @@ int main(int argc, char** argv)
   cv::namedWindow(winname, window_flags);
   cv::setMouseCallback(winname, mouse_callback, NULL);
   //}
-  
+
   ros::Rate r(30);
   const int bot_rows = 100;
   const int right_cols = 100;
@@ -73,10 +71,10 @@ int main(int argc, char** argv)
   while (ros::ok())
   {
     ros::spinOnce();
-  
+
     if (sh_hist->new_data())
     {
-      const mrs_bumper::Histogram hist_msg = sh_hist->get_data();
+      const mrs_msgs::Histogram hist_msg = sh_hist->get_data();
       const int hr = 1000;
       const int hc = 1000;
       cv::Mat disp_im = cv::Mat::zeros(hr + bot_rows, hc + right_cols, CV_8UC3);
